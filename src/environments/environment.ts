493984012dd433b02c33f309783955080be9832c
AugustHagedal <aander23@student.aau.dk>
2025-11-28 11:41:30 +0100

    feat: add Firebase environment configuration files with safe client settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: "secret-diem-vi-project",
+    appId: "1:778645151132:web:bdaf0a1a5367dc41f13c59",
+    storageBucket: "secret-diem-vi-project.firebasestorage.app",
+    apiKey: "AIzaSyCIWwKMTY-U-_7GO--X3zENtXCdYyZi8FI",
+    authDomain: "secret-diem-vi-project.firebaseapp.com",
+    messagingSenderId: "778645151132",
+    measurementId: "G-CFGQ1QVFYG"
+  }
+};
